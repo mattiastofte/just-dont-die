89f32f7eed5b08adb5a43c89e2d76d1824f14bbd
mattiastofte <mattiastofte@gmail.com>
2020-12-13 02:06:44 +0100

    added render entity

--- scripts/rendering_engine.py
+++ scripts/rendering_engine.py
@@ -24,3 +24,8 @@ def Render_Particles(display,particles,time_delta):
     for particle in particles:
         particle.update(time_delta)
         pygame.draw.rect(display, WHITE, particle.rect())
+
+def Render_Entities(display,entities,time_delta,forces):
+    for entity in entities:
+        entity.update(time_delta,forces)
+        pygame.draw.rect(display, (100, 100, 100), entity.rect())
